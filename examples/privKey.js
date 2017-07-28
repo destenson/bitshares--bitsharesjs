e6b83b2758b7ced0245e2b501071463617666c10
Dennis E <git@destenson.digital>
2017-07-28 02:43:57 -0700

    Reformatted all source files with prettier
    # Conflicts:
    #	package.json

--- examples/privKey.js
+++ examples/privKey.js
@@ -1,8 +1,7 @@
-
-import {PrivateKey, key} from "../lib";
+import { PrivateKey, key } from "bitsharesjs";
 
 let seed = "THIS IS A TERRIBLE BRAINKEY SEED WORD SEQUENCE";
-let pkey = PrivateKey.fromSeed( key.normalize_brainKey(seed) );
+let pkey = PrivateKey.fromSeed(key.normalize_brainKey(seed));
 
 console.log("\nPrivate key:", pkey.toWif());
 console.log("Public key :", pkey.toPublicKey().toString(), "\n");
